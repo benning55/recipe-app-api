ac8f60c885e287de9e174e38d001f65a7e55d4c0
benning55 <60070109@kmitl.ac.th>
2019-10-24 21:28:31 +0700

    custom user model

---
+++ app/app/calc.py
@@ -0,0 +1,13 @@
+#
+# def add(x, y):
+#     """
+#     Add Number Together
+#     """
+#     return x+y
+#
+#
+# def subtract(x, y):
+#     """
+#     Subtract x from y
+#     """
+#     return x-y
---
+++ app/app/test.py
@@ -0,0 +1,13 @@
+# from django.test import TestCase
+# from app.calc import add, subtract
+#
+#
+# class CalcTests(TestCase):
+#
+#     def test_add_numbers(self):
+#         """Test that two number are added"""
+#         self.assertEqual(add(3, 8), 11)
+#
+#     def test_subtract_number(self):
+#         """Test that values are subtracted"""
+#         self.assertEqual(subtract(11, 5), 6)
